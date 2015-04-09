5b79fc41c0edf3cb9eaaccd9c74160c619e8c329
Eric Fowler <ericfowler303@gmail.com>
2015-04-09 11:30:59 -0600

    In-class demos from Tuesday and Wednesday

---
+++ TextPrinter/TextPrinter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextPrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextPrinter("Colton's favorite band is nickelback.");
+            Console.ReadKey();
+        }
+        public static void TextPrinter(string inputText)
+        {
+            for (int i = 0; i < inputText.Length; i++)
+            {
+                Console.Write(inputText[i]);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ VowelCounter/VowelCounter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // "test"
+            // "0123"
+            Console.WriteLine("Vowels in the string: " + VowelCounterEasier("test"));
+            Console.ReadKey();
+        }
+        public static int VowelCounter(string inputText)
+        {
+            int counter = 0;
+            for (int i = 0; i < inputText.Length; i++)
+            {
+                if (inputText[i] == 'a' || inputText[i] == 'e' || inputText[i] == 'i' || inputText[i] == 'o' || inputText[i] == 'u')
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+        public static int VowelCounterEasier(string inputText)
+        {
+            int counter = 0;
+            for (int i = 0; i < inputText.Length; i++)
+            {
+                if ("aeiou".Contains(inputText[i]))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
